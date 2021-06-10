2c455177c63b18c4424c06fd3a30277b8023a3df
Nancy <nancy.tsr1@gmail.com>
2021-06-10 18:34:01 +0300

    Initial Commit

---
+++ Assets/DialogueNode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UIElements;
+
+public class DialogueNode
+{
+    public int id { get; set; }
+    public string charName { get; set; }
+    public string dialogueText { get; set; }
+    public int nextNode { get; set; }
+
+    
+}
---
+++ Assets/DialogueResolver.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class DialogueResolver : MonoBehaviour
+{
+    public Text textDisplay;
+    //public List<DialogueNode> dialogueNodes;
+    public Dictionary<int, DialogueNode> dialogueNodes;
+    private int currentDialog = 1;
+    public float typingSpeed;
+    string dialoguesXML = "Assets/dialogues.xml";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadXML();
+        StartCoroutine(Type());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LoadXML()
+    {
+        string xmlString = File.ReadAllText(dialoguesXML);
+        XmlSerializer serializer = new XmlSerializer(typeof(List<DialogueNode>), new XmlRootAttribute("nodes"));
+        StringReader stringReader = new StringReader(xmlString);
+
+        List<DialogueNode> dialogueUnsorted = serializer.Deserialize(stringReader) as List<DialogueNode>;
+        dialogueNodes = dialogueUnsorted.ToDictionary(d => d.id);
+        //dialogueNodes = dialogueUnsorted.OrderBy(q => q.id).ToList();
+
+    }
+
+    void WriteXML()
+    {
+        /*DialogueNode node = new DialogueNode();
+        node.id = 10;
+        node.dialogueText = "Testing serialization";
+        dialogueNodes.Add(node);*/
+        TextWriter writer = new StreamWriter(dialoguesXML);
+        XmlSerializer serializer = new XmlSerializer(typeof(List<DialogueNode>), new XmlRootAttribute("nodes"));
+        serializer.Serialize(writer, dialogueNodes);
+        writer.Close();
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in dialogueNodes[currentDialog].dialogueText.ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void nextSentence()
+    {
+
+        currentDialog = dialogueNodes[currentDialog].nextNode;
+        textDisplay.text = "";
+        if (currentDialog != 0)
+        {
+            StartCoroutine(Type());
+        }
+
+    }
+
+}
